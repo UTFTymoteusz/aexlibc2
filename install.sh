05a15e650106efcc96f2ebd3432d989a88e920b7
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-10-08 18:12:15 +0200

    test

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 GCCPATH=$(which x86_64-aex2-elf-gcc)
+
+if [ -z "$GCCPATH" ] 
+then
+    echo "failed to install the libc"
+    exit 0
+fi
+
 VERSION=$(x86_64-aex2-elf-gcc --version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]')
 DIR=${GCCPATH%/*}
 DST=$DIR/../lib/gcc/x86_64-aex2-elf/$VERSION/

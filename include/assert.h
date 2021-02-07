bcccbc6d9940b392749a23c2ad7d408f93754f21
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2021-02-07 17:53:52 +0100

    assert

---
+++ include/assert.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef NDEBUG
+#define assert(ignore) ((void) 0)
+#else
+#include <stdio.h>
+#define assert(condition)                                                                       \
+    ({                                                                                          \
+        if (!(condition)) {                                                                     \
+            fprintf(stderr, "%s:%i: Assertion failed! (%s)\n", __FILE__, __LINE__, #condition); \
+            abort();                                                                            \
+        }                                                                                       \
+    })
+#endif
